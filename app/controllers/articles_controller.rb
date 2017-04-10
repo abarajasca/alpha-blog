df8598a651f8e662be86139e92248aef4a5cb35b
Adrian Barajas <a_barajas_c@hotmail.com>
2017-04-10 16:33:36 +0000

    Styling errors and messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article was successfully created"
+            flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)    
         else
             render "new"
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
     
     def update
         if @article.update(article_params)
-            flash[:notice] = "Article updated successfully"
+            flash[:success] = "Article updated successfully"
             redirect_to article_path
         else
             render "edit"
@@ -36,7 +36,7 @@ class ArticlesController < ApplicationController
     
     def destroy
         @article.destroy
-        flash[:notice] = "Article deleted successfully"
+        flash[:danger] = "Article deleted successfully"
         redirect_to articles_path
     end
     
